bca21084d321b9d6435e2477a50f37f6d2a375a8
Scott Hain <shain@chef.io>
2018-03-19 12:43:27 -0700

    Change test installation method
    Signed-off-by: Scott Hain <shain@chef.io>

--- scripts/test_project.sh
+++ scripts/test_project.sh
@@ -4,6 +4,6 @@ set -ex
 
 cd ~
 git clone $PROJECT_GIT_URL $PROJECT_NAME
-curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -c unstable -P $PROJECT_NAME -v $PACKAGE_VERSION
+# curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -c unstable -P $PROJECT_NAME -v $PACKAGE_VERSION
 cd /home/omnibus/$PROJECT_NAME
 $VERIFY_COMMAND
